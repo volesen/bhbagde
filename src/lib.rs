85e57f06af524cfd6669053e69432f4a17190bfa
Alexander Krog <moonfrogdk@gmail.com>
2020-08-15 10:35:44 +0200

    shit not working

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,8 @@ pub use hal::samd21::*;
 use hal::clock::GenericClockController;
 #[cfg(feature = "usb")]
 use hal::gpio::IntoFunction;
-use hal::gpio::{Floating, Input, Port, PfC};
-use hal::sercom::{I2CMaster3, PadPin, SPIMaster4, SPIMaster5, UART0};
+use hal::gpio::{Floating, Input, Port};
+use hal::sercom::{I2CMaster2, PadPin};
 use hal::time::Hertz;
 
 #[cfg(feature = "usb")]
@@ -53,9 +53,9 @@ define_pins!(
     /// Pin 2
     pin d2 = a14,
     /// Pin 3, PWM capable
-    pin d3 = a9,
+    pin d3 = a22,
     /// Pin 4, PWM capable.  Also analog input (A8)
-    pin d4 = a8,
+    pin d4 = a23,
     /// Pin 5, PWM capable.  Also analog input (A9)
     pin d5 = a15,
     /// Pin 6, PWM capable
@@ -75,8 +75,9 @@ define_pins!(
     /// Digital pin number 13, which is also attached to
     /// the red LED.  PWM capable.
     pin d13 = a17,
-    pin sda = a22,
-    pin scl = a23,
+
+    pin sda = a8,
+    pin scl = a9,
 
     /// The data line attached to the neopixel.
     /// Is also attached to SWCLK.
@@ -104,151 +105,24 @@ define_pins!(
     pin usb_dp = a25,
 );
 
-/// Convenience for setting up the 2x3 header block for SPI.
-/// This powers up SERCOM4 and configures it for use as an
-/// SPI Master in SPI Mode 0.
-/// Unlike the `flash_spi_master` function, this
-/// one does not accept a CS pin; configuring a pin for CS
-/// is the responsibility of the caller, because it could be
-/// any OutputPin, or even a pulled up line on the slave.
-pub fn spi_master<F: Into<Hertz>>(
-    clocks: &mut GenericClockController,
-    bus_speed: F,
-    sercom4: pac::SERCOM4,
-    pm: &mut pac::PM,
-    sck: gpio::Pb11<Input<Floating>>,
-    mosi: gpio::Pb10<Input<Floating>>,
-    miso: gpio::Pa12<Input<Floating>>,
-    port: &mut Port,
-) -> SPIMaster4<
-        hal::sercom::Sercom4Pad0<gpio::Pa12<gpio::PfD>>,
-        hal::sercom::Sercom4Pad2<gpio::Pb10<gpio::PfD>>,
-        hal::sercom::Sercom4Pad3<gpio::Pb11<gpio::PfD>>
-    > {
-    let gclk0 = clocks.gclk0();
-    SPIMaster4::new(
-        &clocks.sercom4_core(&gclk0).unwrap(),
-        bus_speed.into(),
-        hal::hal::spi::Mode {
-            phase: hal::hal::spi::Phase::CaptureOnFirstTransition,
-            polarity: hal::hal::spi::Polarity::IdleLow,
-        },
-        sercom4,
-        pm,
-        (miso.into_pad(port), mosi.into_pad(port), sck.into_pad(port)),
-    )
-}
-
-/// Convenience for accessing the on-board SPI Flash device.
-/// This powers up SERCOM5 and configures it for use as an
-/// SPI Master.
-pub fn flash_spi_master(
-    clocks: &mut GenericClockController,
-    sercom5: pac::SERCOM5,
-    pm: &mut pac::PM,
-    sck: gpio::Pb23<Input<Floating>>,
-    mosi: gpio::Pb22<Input<Floating>>,
-    miso: gpio::Pb3<Input<Floating>>,
-    cs: gpio::Pa13<Input<Floating>>,
-    port: &mut Port,
-) -> (hal::sercom::SPIMaster5<
-        hal::sercom::Sercom5Pad1<hal::gpio::Pb3<hal::gpio::PfD>>,
-        hal::sercom::Sercom5Pad2<hal::gpio::Pb22<hal::gpio::PfD>>,
-        hal::sercom::Sercom5Pad3<hal::gpio::Pb23<hal::gpio::PfD>>
-    >,
-    hal::gpio::Pa13<hal::gpio::Output<hal::gpio::PushPull>>
-) {
-    let gclk0 = clocks.gclk0();
-    let flash = SPIMaster5::new(
-        &clocks.sercom5_core(&gclk0).unwrap(),
-        48.mhz(),
-        hal::hal::spi::Mode {
-            phase: hal::hal::spi::Phase::CaptureOnFirstTransition,
-            polarity: hal::hal::spi::Polarity::IdleLow,
-        },
-        sercom5,
-        pm,
-        (miso.into_pad(port), mosi.into_pad(port), sck.into_pad(port)),
-    );
-
-    let mut cs = cs.into_push_pull_output(port);
-
-    // We’re confident that set_high won’t error here because on-board
-    // GPIO pins don’t error.
-    cs.set_high().unwrap();
-
-    (flash, cs)
-}
-
 /// Convenience for setting up the labelled SDA, SCL pins to
 /// operate as an I2C master running at the specified frequency.
 pub fn i2c_master<F: Into<Hertz>>(
     clocks: &mut GenericClockController,
     bus_speed: F,
-    sercom3: pac::SERCOM3,
+    sercom2: pac::SERCOM2,
     pm: &mut pac::PM,
-    sda: gpio::Pa22<Input<Floating>>,
-    scl: gpio::Pa23<Input<Floating>>,
+    sda: gpio::Pa8<Input<Floating>>,
+    scl: gpio::Pa9<Input<Floating>>,
     port: &mut Port,
-) -> hal::sercom::I2CMaster3<hal::sercom::Sercom3Pad0<gpio::Pa22<gpio::PfC>>, hal::sercom::Sercom3Pad1<gpio::Pa23<gpio::PfC>>> {
+) -> hal::sercom::I2CMaster2<hal::sercom::Sercom2Pad0<gpio::Pa8<gpio::PfD>>, hal::sercom::Sercom2Pad1<gpio::Pa9<gpio::PfD>>> {
     let gclk0 = clocks.gclk0();
-    I2CMaster3::new(
-        &clocks.sercom3_core(&gclk0).unwrap(),
+    I2CMaster2::new(
+        &clocks.sercom2_core(&gclk0).unwrap(),
         bus_speed.into(),
-        sercom3,
+        sercom2,
         pm,
         sda.into_pad(port),
         scl.into_pad(port),
     )
-}
-
-/// Convenience for setting up the labelled RX, TX pins to
-/// operate as a UART device running at the specified baud.
-pub fn uart<F: Into<Hertz>>(
-    clocks: &mut GenericClockController,
-    baud: F,
-    sercom0: pac::SERCOM0,
-    pm: &mut pac::PM,
-    d0: gpio::Pa11<Input<Floating>>,
-    d1: gpio::Pa10<Input<Floating>>,
-    port: &mut Port,
-) -> UART0<
-    hal::sercom::Sercom0Pad3<gpio::Pa11<PfC>>,
-    hal::sercom::Sercom0Pad2<gpio::Pa10<PfC>>,
-    (),
-    (),
-> {
-    let gclk0 = clocks.gclk0();
-
-    UART0::new(
-        &clocks.sercom0_core(&gclk0).unwrap(),
-        baud.into(),
-        sercom0,
-        pm,
-        (d0.into_pad(port), d1.into_pad(port)),
-    )
-}
-
-#[cfg(feature = "usb")]
-pub fn usb_bus(
-    usb: pac::USB,
-    clocks: &mut GenericClockController,
-    pm: &mut pac::PM,
-    dm: gpio::Pa24<Input<Floating>>,
-    dp: gpio::Pa25<Input<Floating>>,
-    port: &mut Port,
-) -> UsbBusWrapper<UsbBus> {
-    use gpio::IntoFunction;
-
-    let gclk0 = clocks.gclk0();
-    dbgprint!("making usb clock");
-    let usb_clock = &clocks.usb(&gclk0).unwrap();
-    dbgprint!("got clock");
-    UsbBusWrapper::new(UsbBus::new(
-        usb_clock,
-        pm,
-        dm.into_function(port),
-        dp.into_function(port),
-        usb,
-    ))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//http://www.multiwingspan.co.uk/micro.php?page=matdrive
+//https://github.com/adafruit/Adafruit_CircuitPython_IS31FL3731/blob/master/adafruit_is31fl3731.py
+//https://github.com/adafruit/Adafruit_IS31FL3731/blob/master/Adafruit_IS31FL3731.cpp
+
 #![no_std]
 #![no_main]
 
@@ -13,6 +17,136 @@ use hal::delay::Delay;
 use hal::prelude::*;
 use hal::entry;
 use hal::pac::{CorePeripherals, Peripherals};
+use hal::time::KiloHertz;
+use embedded_hal::blocking::i2c;
+
+type Option<T> = core::option::Option<T>;
+
+const _MODE_REGISTER : u8 = 0x00;
+
+const _CONFIG_BANK : u8 = 0x0B;
+const _BANK_ADDRESS : u8 = 0xFD;
+const _PICTURE_MODE : u8 = 0x00;
+
+const ISSI_ADDR_DEFAULT : u8 = 0x74;
+
+const ISSI_REG_CONFIG : u8 = 0x00;
+const ISSI_REG_CONFIG_PICTUREMODE : u8 = 0x00;
+const ISSI_REG_CONFIG_AUTOPLAYMODE : u8 = 0x08;
+const ISSI_REG_CONFIG_AUDIOPLAYMODE : u8 = 0x18;
+
+const ISSI_CONF_PICTUREMODE : u8 = 0x00;
+const ISSI_CONF_AUTOFRAMEMODE : u8 = 0x04;
+const ISSI_CONF_AUDIOMODE : u8 = 0x08;
+
+const ISSI_REG_PICTUREFRAME : u8 = 0x01;
+
+const ISSI_REG_SHUTDOWN : u8 = 0x0A;
+const ISSI_REG_AUDIOSYNC : u8 = 0x06;
+
+const ISSI_COMMANDREGISTER : u8 = 0xFD;
+const ISSI_BANK_FUNCTIONREG : u8 = 0x0B;  // helpfully called 'page nine'
+
+const ADDRESS : u8 = 0x74;
+
+const REG_CONFIG  : u8 = 0x00;
+const REG_CONFIG_PICTUREMODE : u8 = 0x00;
+const REG_CONFIG_AUTOPLAYMODE : u8 = 0x08;
+const REG_CONFIG_AUDIOPLAYMODE : u8 = 0x18;
+
+const CONF_PICTUREMODE : u8 = 0x00;
+const CONF_AUTOFRAMEMODE : u8 = 0x04;
+const CONF_AUDIOMODE : u8 = 0x08;
+ 
+const REG_PICTUREFRAME  : u8 = 0x01;
+
+const REG_SHUTDOWN : u8 = 0x0A;
+const REG_AUDIOSYNC : u8 = 0x06;
+
+const COMMANDREGISTER : u8 = 0xFD;
+const BANK_FUNCTIONREG : u8 = 0x0B; 
+
+const FRAME : u8 = 0x00;
+
+//fn _bank<I2C: i2c::Write>(i2c: &mut I2C, bank: Option<u8>) -> Option<u8> {
+//    let mut result: [u8; 1] = [0];
+//    match bank {
+//        None => {
+//            i2c.write_read(0x74, &[_BANK_ADDRESS], &mut result);
+//            Some(result[0])
+//        }
+//        Some(x) => {
+//            result[0] = x;
+//            i2c.write_read(0x74, &[_BANK_ADDRESS], &mut result);
+//            None
+//        }
+//    }
+//}
+//
+//fn _register<I2C: i2c::Write>(i2c: &mut I2C, bank: u8, register: u8, value: Option<u8>) -> Option<u8> {
+//    _bank(i2c, Some(bank));
+//    let mut result: [u8; 1] = [1];
+//    match value {
+//        None => {
+//            i2c.write_read(0x74, &[register], &mut result);
+//            Some(result[0])
+//        }
+//        Some(x) =>{
+//            result[0] = x;
+//            i2c.write_read(0x74, &[register], &mut result);
+//            None
+//        }
+//    }
+//}
+
+fn write_register<I2C: i2c::Write>(i2c: &mut I2C, reg: u8,  data: u8) {
+   let mut d: [u8; 1] = [reg];
+   i2c.write(0x74, &d);
+   let mut d: [u8; 1] = [data];
+   i2c.write(0x74, &d);
+}
+
+fn select_bank<I2C: i2c::Write>(i2c: &mut I2C, bank: u8){
+    write_register(i2c, COMMANDREGISTER, bank);
+}
+
+fn write_register8<I2C: i2c::Write>(i2c: &mut I2C, bank: u8, reg: u8, value: u8){
+    select_bank(i2c, bank);
+    write_register(i2c, reg, value);
+}
+
+fn set_led_xy<I2C: i2c::Write>(i2c: &mut I2C, x: u8, y: u8, frame: u8, value: u8) {
+    write_register8(i2c, frame, 0x24 + x + y * 16, value);
+}
+
+// fn fill<I2C: i2c::Write>(i2c: &mut I2C, value){
+//     select_bank(i2c, 0);
+//     for i in 0..6{
+//         d = [0x24 + i * 24] + [value] * 24;
+//         i2c.write_read(0x74, d, 0xff)
+//     }
+// }
+
+fn _init<I2C: i2c::Write>(i2c: &mut I2C, delay: &mut Delay) {
+    // Have you tryed to turn it off and on again?
+    write_register8(i2c, BANK_FUNCTIONREG, REG_SHUTDOWN, 0x0);
+    delay.delay_ms(10u32);
+    write_register8(i2c, BANK_FUNCTIONREG, REG_SHUTDOWN, 0x1);
+   
+    // Set picture mode
+    write_register8(i2c, BANK_FUNCTIONREG, REG_CONFIG, REG_CONFIG_PICTUREMODE);
+    write_register8(i2c, BANK_FUNCTIONREG, REG_PICTUREFRAME, FRAME);
+
+    for f in 0..8 {
+        for i in 0..15{
+            write_register8(i2c, f, i, 0xff);
+        }
+    }
+
+    // Turn off audio sync
+    write_register8(i2c, BANK_FUNCTIONREG, REG_AUDIOSYNC, 0x0);
+}
+
 
 #[entry]
 fn main() -> ! {
@@ -27,10 +161,30 @@ fn main() -> ! {
     let mut pins = hal::Pins::new(peripherals.PORT);
     let mut red_led = pins.a1.into_open_drain_output(&mut pins.port);
     let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    //  atsamd8oards/pygamer/examples/neopixel_tilt.rs
+    // https://github.com/BenBergman/lis3dh-rs 
+    let mut i2c = hal::i2c_master(
+        &mut clocks,
+        KiloHertz(400),
+        peripherals.SERCOM2,
+        &mut peripherals.PM,
+        pins.sda,
+        pins.scl,
+        &mut pins.port,
+    );
+    // https://github.com/qmk/qmk_firmware/blob/master/drivers/issi/is31fl3731.c
+
+    _init(&mut i2c, &mut delay);
+
+    // fill(i2c, 127);
+    // set_led_xy(&mut i2c, 5, 5, 0, 240);
+    write_register(&mut i2c, 5, 0xff);
+    
     loop {
-        delay.delay_ms(30u32);
+        delay.delay_ms(100u32);
         red_led.set_high().unwrap();
-        delay.delay_ms(30u32);
+        delay.delay_ms(100u32);
         red_led.set_low().unwrap();
     }
 }
